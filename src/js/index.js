49791b9dc5afb337a118b5b65079e3a706d5061e
yougexiaoluo <1019134495@qq.com>
2019-05-01 14:02:18 +0800

    剩余关于我们这个页面没有做中小屏适配,后续做适配

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+'use-strict';
+// var template = require('../../node_modules/art-template');
+// var html = template(__dirname + '../art/header.art', {
+//   user: {
+//     name: 'aui'
+//   }
+// });
+
+// // 基于模板名渲染模板
+// template(filename, data);
+
+// // 将模板源代码编译成函数
+// template.compile(source, options);
+
+// // 将模板源代码编译成函数并立刻执行
+// template.render(source, data, options);
+
+// console.log(html);
